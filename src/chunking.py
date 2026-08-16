f1fdeba2d60705d179a85b9d22ee2ad9c89bf483
Komal Joshi <komal.joshi.dev@gmail.com>
2026-08-16 19:58:14 +0530

    Improved text chunking strategy

--- src/chunking.py
+++ src/chunking.py
@@ -1,12 +1,13 @@
-def create_chunks(text,chunk_size=100, overlap=20):
+def create_chunks(text,chunk_size=150, overlap=30):
 
+    words = text.split()
     chunks = []
     start = 0
 
-    while start < len(text):
+    while start < len(words):
         end = start + chunk_size
 
-        chunk = text[start : end]
+        chunk = " ".join(words[start : end])
         chunks.append(chunk)
 
         start += chunk_size-overlap
