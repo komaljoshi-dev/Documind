59a683299211915a3a80f5f5751381c4502fede7
Komal Joshi <komal.joshi.dev@gmail.com>
2026-08-18 17:12:35 +0530

    created separate function for query embedding

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,9 +1,11 @@
 from sentence_transformers import SentenceTransformer
 
-
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-def create_embeddings(chunks):
-    return model.encode(chunks)
+def create_chunk_embeddings(chunks):
+    texts = [ chunk["text"] for chunk in chunks]
 
+    return model.encode(texts)
 
+def create_query_embedding(query):
+    return model.encode([query])
